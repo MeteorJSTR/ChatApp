81b07e62d37bc8cbeb6859b214540005bf40eb29
Çağatay ÇALI <me@calicagatay.me>
2016-03-15 07:11:50 +0200

    Relations added

--- collection.js
+++ collection.js
@@ -1,1 +1,23 @@
 var Room = new Mongo.Collection('Room');
+var Post = new Mongo.Collection('Post');
+
+
+/**
+ * Posts has room
+ */
+Post.helpers({
+
+    room: function () {
+        return Room.findOne(this.roomId);
+    }
+});
+
+/**
+ * Room has posts
+ */
+Room.helpers({
+
+    posts: function () {
+        return Post.find({ postId: this._id });
+    }
+});
